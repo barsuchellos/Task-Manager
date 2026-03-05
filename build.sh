1bfec3e9b9aaa4f968648037a22d8df54d74347e
Bohdan Borsuk <barsuchellos@gmail.com>
2026-03-05 11:40:22 +0000

    add superuser creation to build script

--- build.sh
+++ build.sh
@@ -7,3 +7,11 @@ pip install -r requirements/prod.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username=os.environ.get('SUPERUSER_NAME')).exists():
+    User.objects.create_superuser(os.environ.get('SUPERUSER_NAME'), os.environ.get('SUPERUSER_EMAIL'), os.environ.get('SUPERUSER_PASSWORD'))
+"
