b1a0987406e9fd2b82178bdb4ebaa8bb63e819db
Bohdan Borsuk <barsuchellos@gmail.com>
2026-03-05 11:44:50 +0000

    refactor build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ python manage.py shell -c "
 import os
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username=os.environ.get('SUPERUSER_NAME')).exists():
-    User.objects.create_superuser(os.environ.get('SUPERUSER_NAME'), os.environ.get('SUPERUSER_EMAIL'), os.environ.get('SUPERUSER_PASSWORD'))
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, os.environ.get('DJANGO_SUPERUSER_EMAIL'), os.environ.get('DJANGO_SUPERUSER_PASSWORD'))
 "
